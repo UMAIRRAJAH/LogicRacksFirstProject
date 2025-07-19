16c1114f414df1d145c2af658a995d903baf9009
UMAIRRAJAH <yourajah1993@gmail.com>
2025-07-20 04:14:13 +0500

    final work admin data update

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -2,9 +2,11 @@ import express from 'express';
 
 
 import { loginUser,adminLogin,registerUser } from '../controllers/userController.js';
+import { verifyUser } from '../middleware/verifyUser.js';
+
 const userRouter=express.Router();
-userRouter.post('/register',registerUser);
-userRouter.post('/login',loginUser);
-userRouter.post('/admin',adminLogin);
+userRouter.post('/register',verifyUser,registerUser);
+userRouter.post('/login',verifyUser,loginUser);
+userRouter.post('/admin',verifyUser,adminLogin);
 
 export default userRouter;
